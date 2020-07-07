32979827df72a1bc62cb626e60ad7e307c3691ed
Stephen <stephen_h369@live.co.uk>
2020-07-07 19:51:24 +0100

    added links for each list item, each item links to its own .erb

--- controller.rb
+++ controller.rb
@@ -8,3 +8,23 @@ get '/films' do
     @films = Film.all()
     erb(:index)
 end
+
+get '/Ghost' do
+    @films = Film.all()
+    erb(:film1)
+end
+
+get '/The Sixth Sense' do
+    @films = Film.all()
+    erb(:film2)
+end
+
+get '/Beetlejuice' do
+    @films = Film.all()
+    erb(:film3)
+end
+
+get '/Casper' do
+    @films = Film.all()
+    erb(:film4)
+end
